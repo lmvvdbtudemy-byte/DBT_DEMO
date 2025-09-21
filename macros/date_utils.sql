5e7a2733513a18497a881732f4d6050f56c78c12
Laura Vergara <lmvergara@gmail.com>
2025-09-21 10:13:15 +0000

    daily_weather_model created

---
+++ macros/date_utils.sql
@@ -0,0 +1,17 @@
+{% macro get_season(x) %}
+
+case when month(to_timestamp({{x}})) in (12,1,2) then 'WINTER'
+     when month(to_timestamp({{x}})) in (3,4,5) then 'SPRING'
+     when month(to_timestamp({{x}})) in (6,7,8) then 'SUMMER'
+     else 'AUTUMN' END
+
+
+{% endmacro %}
+
+{% macro day_type(x) %}
+
+case when dayname(to_timestamp({{x}})) in ('Sat','Sun')
+  then 'WEEKEND'
+  ELSE 'BUSINESSDAY' END
+
+{% endmacro %}
